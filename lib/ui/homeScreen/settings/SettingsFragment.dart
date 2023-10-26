4915e9ade426565467e779cafa7af6e38a89bcee
BasilAhmedAbdellah <basilbeso2006@gmail.com>
2023-10-26 12:12:43 +0300

    adding empty bottom sheets

--- lib/ui/homeScreen/settings/SettingsFragment.dart
+++ lib/ui/homeScreen/settings/SettingsFragment.dart
@@ -1,12 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:news/ui/homeScreen/settings/LanguageBottomSheet.dart';
 
-class SettingsFragment extends StatelessWidget {
+class SettingsFragment extends StatefulWidget {
   const SettingsFragment({super.key});
 
   @override
+  State<SettingsFragment> createState() => _SettingsFragmentState();
+}
+
+class _SettingsFragmentState extends State<SettingsFragment> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      color: Colors.black,
+    return Scaffold(
+     body: Padding(
+       padding: const EdgeInsets.all(18.0),
+       child: Column(
+         crossAxisAlignment: CrossAxisAlignment.stretch,
+         children: [
+           InkWell(
+             onTap: (){
+               showLanguageBottomSheet();
+             },
+             child: Container(
+               width: double.infinity,
+               padding: EdgeInsets.all(12),
+               decoration: BoxDecoration(
+                 color: Theme.of(context).colorScheme.background,
+                 borderRadius: BorderRadius.circular(12),
+                 border: Border.all(color: Theme.of(context).dividerColor,
+                     width: 1),
+
+               ),
+               child: Text("Language"),
+             ),
+           ),
+         ],
+       ),
+     ),
+    );
+
+  }
+  void  showLanguageBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return LanguageBottomSheet();
+      },
     );
   }
 }
+
+
